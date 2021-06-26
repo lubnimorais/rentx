4585dafd62d60779bee3083f33825147f379aec6
Lubni Morais <lubni.morais@gmail.com>
2021-06-25 22:20:37 -0300

    chore: Added lib cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
@@ -19,6 +20,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
